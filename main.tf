ee22b04c7e405b3efac08062d791e4687699cfc2
chasehippen <chase@hippen.org>
2023-02-21 17:05:52 -0800

    cloud custodian not compatible with fifo queue

--- main.tf
+++ main.tf
@@ -106,10 +106,9 @@ resource "aws_lambda_function" "handler_lambda" {
 }
 
 resource "aws_sqs_queue" "event_sqs_queue" {
-  name = "event-handler-${var.name}.fifo"
+  name = "event-handler-${var.name}"
 
   kms_master_key_id = var.kms_master_key_id == "" ? null : var.kms_master_key_id
-  fifo_queue        = true
 
   tags = {
     owner      = var.owner
